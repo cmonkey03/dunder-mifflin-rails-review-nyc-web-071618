a4d772ddca0b9fac1286eaf075e4adb8eae6177b
cmonkey03 <russellpbrown@gmail.com>
2018-08-16 17:15:50 -0400

    Almost finished deliverables

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,30 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find_by_id(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    if @employee.save
+      redirect_to employee_path(@employee)
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def employee_params
+      params.require(:employee).permit(:first_name, :last_name, :alias, :title, :image_url, :dog_id)
+    end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,10 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+  validates :alias, uniqueness: true
+  validates :title, uniqueness: true
+
+  def to_s
+    last_name + " " + first_name
+  end
+
 end
